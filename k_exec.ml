eb83486fc421b864ee69a07f1e7d12fe3cac506f
Matthew Maurer <maurer@matthewmaurer.org>
2015-03-19 19:58:03 +0000

    First draft of concretizing k_exec

--- k_exec.ml
+++ k_exec.ml
@@ -27,11 +27,9 @@ let init_step k u = {
   term  = false;
 }
 
-type 'a trace_func = insn -> model -> 'a -> 'a trace_step
-
-let run_insn (model : model) (insn : insn) : model = model
-
 module Dis = Disasm_expert.Basic
+
+type 'a trace_func = Dis.full_insn -> stmt list -> 'a trace_step -> 'a trace_step
 
 let opt_error msg = function
   | None -> Or_error.error_string msg
@@ -44,7 +42,29 @@ type term_cond =
   | K_term
   | Unmapped_term
 
-let exec_insn _ _ s = s
+exception Abort of string
+
+let exec_insn f (mem, opt_insn) (s, _, _) =
+  if (s.term) then (s, [], false) else
+  let inval_dis_msg = "Invalid disasms terminate. They should not be here." in
+  let insn = Option.value_exn ~message:inval_dis_msg opt_insn in
+  match Bap_disasm_arm_lifter.insn mem insn with
+   | Ok(bil) ->
+       let (c', next) = Conceval.eval_stmts (s.model.conc) bil in
+       let (tgts, fall) = (match next with
+          | Some(Conceval.BV tgt) -> ([tgt], false)
+          | Some(Conceval.Mem _) -> raise (Abort "memory bank as jump target, invalid lift")
+          | Some(Un(_,_))
+          | None -> ([], true)) in
+       let s' = {s with model = {s.model with conc = c'}} in
+       let s'' = f insn bil s' in
+       (s'', tgts, fall)
+   | Error(_) -> 
+       (* This isn't quite right, since it will report user_term *)
+       (* I should find a way to make invalid_term happen instead *)
+       ({s with term = true}, [], false)
+    
+   
 
 let k_exec image ~start ~k ~f ~init:u_init =
   (* By the invariant of find_addr, Memory.view should not error *)
@@ -58,8 +78,8 @@ let k_exec image ~start ~k ~f ~init:u_init =
   Dis.run dis mem ~stop_on:[`May_affect_control_flow] 
     ~return:(fun _ -> return ())
     ~init
-    ~invalid:(fun d mem s -> yield (Invalid_term, s.state))
-    ~hit:(fun d mem insn s ->
+    ~invalid:(fun _ _ s -> yield (Invalid_term, s.state))
+    ~hit:(fun d _ _ s ->
       if s.k = 0 then yield (K_term, s.state) else
       (* Run the insn sequence *)
       let (s, tgts, fall) = List.fold_right (Dis.insns d) ~f:(exec_insn f) ~init:(s, [], false) in
@@ -67,7 +87,7 @@ let k_exec image ~start ~k ~f ~init:u_init =
       let s = {s with k = s.k - 1} in
       if s.term then yield (User_term, s.state) else
       (* Hit all jump targets *)
-      List.fold_left tgts ~f:(fun tgt m -> m >>= fun _ ->
+      List.fold_left tgts ~f:(fun m tgt -> m >>= fun _ ->
         if AS.mem s.past tgt then return () else
         match get_mem tgt with
           | Some mem -> Dis.jump d mem {s with past = AS.add s.past tgt}
@@ -78,4 +98,4 @@ let k_exec image ~start ~k ~f ~init:u_init =
         then Dis.step d s
         else return ()
     )
-    ~stopped:(fun d s -> yield @@ (Early_term, s.state)) |> run |> Or_error.return
+    ~stopped:(fun _ s -> yield @@ (Early_term, s.state)) |> run |> Or_error.return
